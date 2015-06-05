f35edc48f38f97d6be5da8df584473b2917af284
sarvesh-onlyme <sarvesh.onlyme@gmail.com>
2015-06-05 14:08:51 +0530

    Editable text implemented

--- js/Crtl.js
+++ js/Crtl.js
@@ -1,6 +1,7 @@
 app.controller("wikiCtrl", function($scope, $http){
 	$http.get("./data/contributors.json").success(function (response) {
 		$scope.data = response;
+		contributorsList = response;
 	});
 });
 
@@ -23,19 +24,45 @@ app.controller("homeCtrl", function($scope, $routeParams){
 
 	$scope.showEditRow = function(x) {
 		var id = this.x.id;
-		console.log(id);
-		$("[name="+id+"]").$scope.active = 1;
-//		$scope.active = 1;
-//		$scope.active = !$scope.active;
+		if(!this.active) {
+			$("[name="+id+"] > td > input").removeClass("ng-hide");
+			$("[name="+id+"] > td > span").addClass("ng-hide");
+			$("[name="+id+"] > td > a:first-child > span").text("Save");
+		}
+		else {
+			$("[name="+id+"] > td > input").addClass("ng-hide");
+			$("[name="+id+"] > td > span").removeClass("ng-hide");
+			$("[name="+id+"] > td > a:first-child > span").text("Edit");
+
+			/* Save edited data*/
+			var newData = {};
+			for (var i = 0; i < attributes.length; i++) {
+				var value = $("[name="+id+"] [name="+attributes[i]+"] input").val();
+				newData[attributes[i]] = value; 	
+			};
+
+			/* Send newData to backend */
+			console.log(newData);
+		}
+		this.active = !this.active;
 	};
+
+	$scope.deleteRow = function(x) {
+		var id = this.x.id;
+		
+		/* Send id to backend */
+		console.log(id);
+	}
 });
 
 app.controller("accountCtrl", function($scope, $routeParams, $http){
 	$scope.param = $routeParams.param;
-	/* Get user's details from backend
-	$http.get("http://localhost/templates/data/contributors.json").success(function (response) {
+	
+	/* Get user's details from backend */
+	/*$http.get("http://localhost/templates/data/contributors.json").success(function (response) {
 		console.log(response);
 	});*/
+
 	user = {
 		"id": 1,
 		"name": "john",
@@ -47,6 +74,31 @@ app.controller("accountCtrl", function($scope, $routeParams, $http){
 		"end_date": "currently"
 	};
 	$scope.user = user;
+
+	$scope.showEditRow = function(x) {
+		if(!this.active) {
+			$("table > td > input").removeClass("ng-hide");
+			$("table > td > span").addClass("ng-hide");
+			$(".edit > a:first-child > span").text("Save");
+		}
+		else {
+			$("name > td > input").addClass("ng-hide");
+			$("name > td > span").removeClass("ng-hide");
+			$(".edit > a:first-child > span").text("Edit");
+
+			/* Save edited data*/
+			var newData = {};
+			for (var i = 0; i < attributes.length; i++) {
+				var value = $("[name="+attributes[i]+"] input").val();
+				newData[attributes[i]] = value; 	
+			};
+
+			/* Send newData to backend */
+			console.log(newData);
+		}
+		this.active = !this.active;
+	};
+
 });
 
 app.controller("signinCtrl", function($scope, $routeParams){
--- js/wikiApp.js
+++ js/wikiApp.js
@@ -1,1 +1,3 @@
 var app = angular.module("wikiApp", ['ngRoute']);
+var contributorsList = null;
+var attributes = ["id", "name", "username", "email", "organization", "country", "start_date", "end_date"];
